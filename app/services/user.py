0210a0df45d67ff8ca133c9c51d51e94f2326d88
Urielglb <uriel_06@ciencias.unam.mx>
2024-09-18 19:37:05 -0600

    :sparkles: Changing withdraw to work with pin

--- app/services/user.py
+++ app/services/user.py
@@ -6,6 +6,10 @@ def get_user_by_bank_account(session: Session, bank_account: str) -> User:
     return session.exec(select(User).where(User.bank_account == bank_account)).first()
 
 
+def get_user_by_pin(session: Session, pin: str) -> User:
+    return session.exec(select(User).where(User.pin == pin)).first()
+
+
 def update_user_balance(session: Session, user: User, new_balance: float) -> User:
     user.balance = new_balance
     session.add(user)
@@ -14,8 +18,8 @@ def update_user_balance(session: Session, user: User, new_balance: float) -> Use
     return user
 
 
-def withdraw(session: Session, bank_account: str, amount: float) -> User:
-    user = get_user_by_bank_account(session, bank_account)
+def withdraw(session: Session, pin: str, amount: float) -> User:
+    user = get_user_by_pin(session, pin)
     if user and user.balance >= amount:
         new_balance = user.balance - amount
         return update_user_balance(session, user, new_balance)
