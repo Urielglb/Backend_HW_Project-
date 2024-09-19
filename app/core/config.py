9b4543ac926c9d1eceec3ca15c2cedde8062277d
Urielglb <uriel_06@ciencias.unam.mx>
2024-09-18 19:44:00 -0600

    :fire: unused env variables

--- app/core/config.py
+++ app/core/config.py
@@ -6,9 +6,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     API_STR: str = "/api"
-    SECRET_KEY: str = secrets.token_urlsafe(32)
-    ALGORITHM: str
-    SERVER_HOST: AnyHttpUrl
     BACKEND_CORS_ORIGIN: list[AnyHttpUrl] | str = []
 
     @field_validator("BACKEND_CORS_ORIGIN", mode="before")
